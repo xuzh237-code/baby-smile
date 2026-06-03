93823214a3db870cf7189b79ebd897b33c295c96
xuzh237-code <xuzh237@gmail.com>
2026-06-03 22:58:38 +0800

    Configure mini program cloud env

--- miniprogram/utils/cloudConfig.js
+++ miniprogram/utils/cloudConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 在微信开发者工具「云开发」里创建环境后，把环境 ID 填到这里。
   // 留空时小程序仍然完整支持本地记录，只是不会启用微信云同步。
-  CLOUD_ENV_ID: '',
+  CLOUD_ENV_ID: 'cloud1-d4glphb454bbae926',
   SYNC_COLLECTION: 'baby_smile_sync'
 };
